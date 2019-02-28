dec5ee77dfc421ecc1c8a849b9a5e490b4709838
Ali Lahrime <alahrime@email.wm.edu>
2019-02-28 16:12:27 -0500

    editbookmark template

---
+++ bookmarks-app/src/EditBookmark/EditBookmark.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class EditBookmarkForm extends Component {
+  /* state for inputs etc... */
+  render() {
+    return (
+      <section className="EditBookmarkForm">
+        <h2>Edit Bookmark</h2>
+        <form>{/* inputs etc... */}</form>
+      </section>
+    );
+  }
+}
